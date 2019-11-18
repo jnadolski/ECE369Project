0e2d8f344129a514147b8d2773a0b14dd6057ab0
jnadolski <nadolskj@email.arizona.edu>
2019-11-18 16:33:28 -0700

    forwardingunit created

---
+++ ece369finalproject.srcs/sources_1/new/ForwardingUnit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/18/2019 04:33:03 PM
+// Design Name: 
+// Module Name: ForwardingUnit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ForwardingUnit(
+
+    );
+endmodule
