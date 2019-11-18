a53531c12e37e2abcb5fabdfe9c0b39cf6eceb18
jnadolski <nadolskj@email.arizona.edu>
2019-11-18 16:13:25 -0700

    test datapath tb for directories

---
+++ ece369finalproject.srcs/sim_1/new/Datapath_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/18/2019 04:12:55 PM
+// Design Name: 
+// Module Name: Datapath_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Datapath_tb(
+
+    );
+endmodule
