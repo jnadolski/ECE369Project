86d6d48d9c92b8e0ba54e6f21124c765e07e9995
jnadolski <nadolskj@email.arizona.edu>
2019-11-27 16:39:27 -0700

    tried to synthize

--- ece369finalproject.srcs/sources_1/new/IFIDRegister.v
+++ ece369finalproject.srcs/sources_1/new/IFIDRegister.v
@@ -2,16 +2,23 @@
 
 //Jennifer Nadolski / Joseph Emnett
 
-module IFIDReg(PCValue, InstructionRead,Clk, ReadDataPCValue, ReadDataInstructionRead);
+module IFIDReg(PCValue, InstructionRead,Clk, ReadDataPCValue, ReadDataInstructionRead, flushcontrol);
 
 	input [31:0] PCValue, InstructionRead;
 	input Clk;
+	input flushcontrol;
 	output reg [31:0] ReadDataPCValue, ReadDataInstructionRead;
 	
 	    always @(posedge Clk) begin
-        
+        if (flushcontrol ==1'b0)begin
          ReadDataPCValue <= PCValue;
          ReadDataInstructionRead <= InstructionRead;
+        end
+        else if (flushcontrol ==1'b1)begin
+        ReadDataPCValue <=32'b00000000;
+        ReadDataInstructionRead <=32'b00000000;
+        end
+        
    
     end
  
