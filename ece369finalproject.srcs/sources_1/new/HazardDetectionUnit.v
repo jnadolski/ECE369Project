c01d888ecddc9156e41bdc95ca59a3f4105e6e26
jcemnett <57923384+jcemnett@users.noreply.github.com>
2019-12-01 16:46:15 -0700

    update hazard unit

--- ece369finalproject.srcs/sources_1/new/HazardDetectionUnit.v
+++ ece369finalproject.srcs/sources_1/new/HazardDetectionUnit.v
@@ -1,34 +1,39 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/18/2019 04:08:51 PM
-// Design Name: 
-// Module Name: HazardDetectionUnit
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module HazardDetectionUnit(Branch,Jump, PCWrite, RS, RD, IFIDWrite, exmemregister, WriteBackRegWrite, WriteDataRegister, idexmemread,  flushcontrol, stallcontrol, hilocontrol, controllercontrol);
 
-
-module HazardDetectionUnit(branch, flushcontrol);
-    input branch;
-    output reg flushcontrol;
-    
-    always @(branch) begin
-        if(branch == 1)begin
-            flushcontrol <= 1'b1;
+    input Branch, Jump, WriteBackRegWrite, idexmemread;
+    output reg flushcontrol, stallcontrol, hilocontrol, PCWrite, IFIDWrite, controllercontrol;
+    input [4:0] RS, RD, exmemregister, WriteDataRegister;
+    always @(*) begin
+        if(Branch == 1)begin
+            flushcontrol <=1'b1;
+            controllercontrol <=1'b1;
+            hilocontrol <= 1'b1;    //keeps the hilo data 
+            stallcontrol <=1'b0;     //doesn't stall for branch 
         end
-            
+        else if (Jump == 1)begin
+            flushcontrol <=1'b1;
+            controllercontrol <=1'b1;
+            hilocontrol <= 1'b1;    //keeps the hilo data 
+            stallcontrol <=1'b0;     //doesn't stall for jumps 
+        end
+        
+        else if(idexmemread == 1'b1 && (RS == WriteDataRegister || RD == WriteDataRegister) && (WriteDataRegister !=5'b00000))begin
+            stallcontrol <= 1'b1;    //stalls 
+            controllercontrol <=1'b1;
+            PCWrite <=1'b1;         //PC won't count the next addr 
+        end
+        else if (WriteBackRegWrite ==1'b1 && (RS == exmemregister && RD == exmemregister) && exmemregister != 5'b00000) begin
+            stallcontrol <= 1'b1;    //stalls 
+            controllercontrol <=1'b1;
+            PCWrite <=1'b1;         //PC won't count the next addr 
+        end
+        else begin
+            flushcontrol <=1'b0;
+            hilocontrol <= 1'b0;    //lets alu write over hilo data
+            stallcontrol <=1'b0;     //doesnt stall  
+            controllercontrol <=1'b0;
+            PCWrite <=1'b0;
+        end
     end
     
     
